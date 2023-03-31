a72a9200ef9152547ff3fb3d1d035813ce3e7734
Doudmur <57396825+Doudmur@users.noreply.github.com>
2023-03-31 08:45:25 +0600

    Update main.go

--- lab7/main.go
+++ lab7/main.go
@@ -4,13 +4,17 @@ import (
 	"fmt"
 )
 
+func cube(num int) int {
+	return num * num * num
+}
+
 func main() {
 	var a, sum int
 	fmt.Scan(&a)
 	arr := make([]int, a)
 	for i := 0; i < a; i++ {
 		fmt.Scan(&arr[i])
-		sum += arr[i] * arr[i] * arr[i]
+		sum += cube(arr[i])
 	}
 	fmt.Print(sum)
 }
